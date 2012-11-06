e60c4b65da511b3af89bdb0cd95afe53bdc7496c
Laurenz <laurenz.glueck@me.com>
2012-11-06 20:56:52 +0100

    Add refresh Action

--- HTML 5 Demo/ViewController.h
+++ HTML 5 Demo/ViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    IBOutlet UIWebView *webview;
+    
+    
+}
+
+-(IBAction)refresh:(id)sender;
+
+
+
+
+// @property(nonatomic,retain) UIWebView *webView;
 
 @end
